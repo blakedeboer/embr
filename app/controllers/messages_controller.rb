6ede789c65b03ece1e579965a7d76744d2a615c5
blakedeboer <deboer.blake@gmail.com>
2014-11-17 10:43:53 -0500

    tweaked the back end

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,7 @@ class MessagesController < ApplicationController
   end
 
   def create
-    message = Message.new(:to_id => params[:user_id], :from_id => params[:likee_id], :content => params[:content])
+    message = Message.new(:to_id => params[:likee_id], :from_id => params[:user_id], :content => params[:content])
     message.save
     redirect_to user_message_path(params[:user_id], params[:likee_id])
   end
